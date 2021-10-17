7c223f1150cef4d5ebcf868ee47771a2a17e315b
stac3y <stacy.starina@gmail.com>
2021-10-17 22:30:29 +0300

    feat: add route for getting random joke

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import express from 'express'
+import axios from 'axios'
 
 const app = express();
 const port = 3000;
+
+app.get('/', ((req, res) => {
+    let data : object;
+    axios.get('https://v2.jokeapi.dev/joke/Any')
+        .then(res => {
+           data = <object>res.data;
+            console.log(data)
+        });
+    res.send(data)
+}))
 
 app.listen(port, () => console.log(`Running on port ${port}`));
