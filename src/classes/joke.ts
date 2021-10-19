9cf738090416b0eb4ae9bc935c2822128969eee0
stac3y <stacy.starina@gmail.com>
2021-10-19 22:28:18 +0300

    feat: add function for getting a joke by category

--- src/classes/joke.ts
+++ src/classes/joke.ts
@@ -3,7 +3,14 @@ import axios from 'axios'
 export class Joke {
     static async getRandomJoke() {
         const res = await axios.get(
-            'https://v2.jokeapi.dev/joke/Any?type=single',
+            `https://v2.jokeapi.dev/joke/Any?type=single`,
+        )
+        return res.data['joke']
+    }
+
+    static async getJokeByCategory(category: string) {
+        const res = await axios.get(
+            `https://v2.jokeapi.dev/joke/${category}?type=single`,
         )
         return res.data['joke']
     }
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,22 @@ import { Joke } from './classes/joke'
 
 const program = new Command()
 
-program.version('1.0.0').description('Configuration files creator.')
+program.version('1.0.0').description('Console application for jokes.')
 program.option('-r, --random', 'output random joke')
+program.option('-c, --category <category>')
 
 program.parse(process.argv)
 
 const options = program.opts()
+
 if (options.random) {
     Joke.getRandomJoke().then((joke) => {
+        console.log(joke)
+    })
+}
+
+if (options.category) {
+    Joke.getJokeByCategory(options.category).then((joke) => {
         console.log(joke)
     })
 }
