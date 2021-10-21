c5adec70ea93bf4f5ebaa9a4bf487868f8155169
stac3y <stacy.starina@gmail.com>
2021-10-21 14:27:00 +0300

    docs: add TSDocs

--- src/classes/joke.ts
+++ src/classes/joke.ts
@@ -3,6 +3,12 @@ import { performance } from 'perf_hooks'
 import axios from 'axios'
 
 export class Joke {
+    /**
+     * Function for geting a joke by category with async/await
+     *
+     * @param category - category of a joke
+     * @returns promise with a joke
+     */
     static async getJokeWithAsync(category: string = 'Any'): Promise<string> {
         const res = await axios.get(
             `https://v2.jokeapi.dev/joke/${category}?type=single`,
@@ -10,6 +16,12 @@ export class Joke {
         return res.data['joke']
     }
 
+    /**
+     *   Function for geting a joke by category with promise.then().catch()
+     *
+     * @param category - category of a joke
+     * @returns promise with a joke
+     */
     static getJokeWithPromise(category: string = 'Any'): Promise<string> {
         return axios
             .get(`https://v2.jokeapi.dev/joke/${category}?type=single`)
@@ -21,6 +33,12 @@ export class Joke {
             })
     }
 
+    /**
+     * Function to check if a category matches the possible categories
+     *
+     * @param category - category of a joke
+     * @returns true if category exists or false if don't
+     */
     static checkCategory(category: string): boolean {
         const categories = [
             'Any',
@@ -40,6 +58,11 @@ export class Joke {
         } else return true
     }
 
+    /**
+     * Function for running async test with promises
+     *
+     * @param n - number of iterations
+     */
     static async asyncTest(n: number) {
         let i: number = 0
         let promises: Array<Promise<string>> = []
@@ -58,6 +81,11 @@ export class Joke {
         })
     }
 
+    /**
+     * Function for running sync test
+     *
+     * @param n - number of iterations
+     */
     static async syncTest(n: number) {
         let i: number = 0
         const beginTime = performance.now()
